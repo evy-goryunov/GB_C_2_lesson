90f9c43a5d2a18989881073946ec9852f6d4f6b7
murder_face <murderface.bot@gmail.com>
2019-04-21 21:45:59 +0300

    сделал 2 задачу

---
+++ Hometask_2_2/Hometask_2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2. Написать метод подсчета количества цифр числа.
+
+namespace Hometask_2_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите число");
+			String number = Console.ReadLine();
+			Console.WriteLine($"Цифр в числе: {Counter(number)}");
+			Console.ReadKey();
+		}
+
+		public static int Counter(String num)
+		{
+			int count = num.Length;
+			return count;
+		}
+	}
+}
