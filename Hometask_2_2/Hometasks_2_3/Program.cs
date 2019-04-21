237c9a679d3275f91ef5703538c2d18f698f5942
evy-goryunov <evy.goryunov@gmail.com>
2019-04-21 22:01:05 +0300

    сделал третью задачу

---
+++ Hometask_2_2/Hometasks_2_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+namespace Hometasks_2_3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = 0;
+			Console.WriteLine("Вводите числа чтобы узнать их сумму нечетных положительных чисел");
+			while(true)
+			{
+				int num = Convert.ToInt32(Console.ReadLine());
+				if (num > 0 && num % 2 != 0) count = count + num;
+				else if (num == 0)
+				{
+					break;
+				}
+			}
+			Console.WriteLine($"Сумма нечётных положительных чисел: {count}");
+			Console.ReadKey();
+		}
+	}
+}
